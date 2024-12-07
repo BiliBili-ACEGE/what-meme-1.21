34c5ac930bddbfce171c2db67c05028cc60fd835
Ace哥 <3486598566@qq.com>
2024-12-07 13:30:47 +0800

    1.21

--- src/main/java/net/ace/WhatMeme.java
+++ src/main/java/net/ace/WhatMeme.java
@@ -6,11 +6,9 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-import static net.ace.whatmeme.sounds.SoundManager.WHAT_SOUND_EVENT;
 import static net.ace.whatmeme.sounds.SoundManager.registerSounds;
 
 public class WhatMeme implements ModInitializer {
